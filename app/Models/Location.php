4a649a6ff604c84ae2510b68b2bd76e619a63855
John Chishugi <jkchishugi@gmail.com>
2021-05-15 13:53:27 +0100

    ft-set-relation-hasMany-location-and-lockers

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Location extends Model
 {
     use HasFactory;
+
+    public function lockers(){
+        return $this->hasMany(Locker::class);
+    }
 }
