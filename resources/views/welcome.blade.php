dc1023b5de3e51f5296b767614f710c591240f38
John Chishugi <jkchishugi@gmail.com>
2021-05-15 16:37:53 +0100

    fx-code-style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,78 +1,83 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Find Lockers</title>
-         <style>
-            html, body {
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100%;
-                margin: 0;
-            }
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-            .gradient{
-                background-color: rgba(0,0,0,0.7);
-                /*opacity: 0.7;*/
-                color: #fff;
-                position: absolute;
-                z-index: 15;
-            }
+    <title>Find Lockers</title>
+    <style>
+        html,
+        body {
+            font-family: 'Raleway', sans-serif;
+            font-weight: 100;
+            height: 100%;
+            margin: 0;
+        }
 
-            .gradient .links{
-                color: red;
-            }
+        .gradient {
+            background-color: rgba(0, 0, 0, 0.7);
+            /*opacity: 0.7;*/
+            color: #fff;
+            position: absolute;
+            z-index: 15;
+        }
 
-            .full-height {
-                height: 100vh;
-            }
+        .gradient .links {
+            color: red;
+        }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+        .full-height {
+            height: 100vh;
+        }
 
-            .position-ref {
-                position: relative;
-            }
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+        .position-ref {
+            position: relative;
+        }
 
-            .content {
-                text-align: center;
-            }
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
 
-            .title {
-                font-size: 70px;
-                z-index: 100;
-                opacity: 0.9;
-                position: relative;
-            }
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 70px;
+            z-index: 100;
+            opacity: 0.9;
+            position: relative;
+        }
 
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        
-         @livewireStyles
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-    </head>
-    <body>
-      <div class="row mt-5">
-            <div class="col-md-6 mx-auto">
-                 @livewire('live-search')
-            </div>
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+
+    </style>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    @livewireStyles
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="row mt-5">
+        <div class="col-md-6 mx-auto">
+            @livewire('live-search')
         </div>
-        @livewireScripts
-    </body>
+    </div>
+    @livewireScripts
+</body>
+
 </html>
