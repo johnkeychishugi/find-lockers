8e9dcd311c5bac852f92f9c9333d25a55c23eb4f
John Chishugi <jkchishugi@gmail.com>
2021-05-15 14:38:18 +0100

    ft-initialization-of-livewire

---
+++ app/Http/Livewire/LiveSearch.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class LiveSearch extends Component
+{
+    public function render()
+    {
+        return view('livewire.live-search');
+    }
+}
