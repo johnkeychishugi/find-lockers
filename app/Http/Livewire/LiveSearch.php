c56cdc231d6911c03856624511397af6a92225e8
John Chishugi <jkchishugi@gmail.com>
2021-05-15 15:56:31 +0100

    ft-fetch-data-from-database

--- app/Http/Livewire/LiveSearch.php
+++ app/Http/Livewire/LiveSearch.php
@@ -8,11 +8,26 @@ use Livewire\Component;
 class LiveSearch extends Component
 {
     public $name;
+	public $contacts = [];
+	public $loading = false;
+
+    public function updatedName($value)
+    {
+        if(!empty($this->name)){
+			sleep(1);
+			
+			$results = $this->contacts = Location::searchByName($value);
+
+			if(empty($results)){
+				session()->flash('message','No contact matching '.$this->name. '.');
+			}
+
+		    $this->contacts = $results;
+		}	
+    }
 
     public function render()
     {  
-        return view('livewire.live-search',[
-           'contacts' =>  Location::searchByName('e')
-        ]);
+        return view('livewire.live-search');
     }
 }
