917d77b6ae67d713f8d8eba1cfe05230f661d129
John Chishugi <jkchishugi@gmail.com>
2021-05-18 13:03:22 +0100

    ft-menu-done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,33 +15,45 @@
 </head>
 
 <body>
-      <nav class="navbar navbar-expand-lg navbar-light bg-nav">
+    <nav class="navbar navbar-expand-lg navbar-light bg-nav">
         <a class="navbar-brand" href="#">
             <img src="{{ asset('images/logo.png') }}" width="150" height="50" class="d-inline-block align-top" alt="">
-          </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav nav-list">
-            <li class="nav-item">
-              <a class="nav-link" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Find a Locker</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Size Guide</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Locations</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Help Center</a>
-              </li>
-          </ul>
+            <ul class="navbar-nav nav-list">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Find a Locker</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Size Guide</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Locations</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Help Center</a>
+                </li>
+            </ul>
         </div>
-      </nav>
+        <ul class="navbar-nav nav-list justify-content-end">
+            <li class="nav-item">
+                <a class="nav-link" href="#">My Account</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">080-188-0872</a>
+            </li>
+            <li class="nav-item paybill">
+                <a class="nav-link" href="#">PAY YOUR BILL</a>
+            </li>
+        </ul>
+    </nav>
     <div class="row mt-5">
         <div class="col-md-6 mx-auto">
             @livewire('live-search')
